ca6938a465df0b14b48557a228d0a803013c9e0b
elias-prodigy <ilyazp90@gmail.com>
2020-02-18 16:57:40 +0200

    preparing proj for heroku

--- Party_maker/settings.py
+++ Party_maker/settings.py
@@ -176,3 +176,7 @@ EMAIL_PORT = 587
 prod_db = dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(prod_db)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
